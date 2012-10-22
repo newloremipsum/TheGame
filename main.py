8c36778e3158782e845c5a35e62aec4e8f139fe8
Anton Mordberg <antonio24@yandex.ru>
2012-10-23 00:37:06 +0400

    create a matrix with buttons

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-import string
+#!/usr/bin/python2.7
 
-print 'qt'.upper()
+import sys
+from PyQt4 import QtGui, QtCore
+
+class TestGui(QtGui.QDialog):
+    def __init__(self):
+        QtGui.QDialog.__init__(self)
+
+        self.setGeometry(600, 300, 400, 400)
+        self.setWindowTitle("The")
+        btnLines = []
+        for j in range(10):
+            btnLines.append([QtGui.QPushButton(str(i) + ';' + str(j), self) for i in xrange(10)])
+            for i in range(10):
+                btnLines[j][i].setGeometry(i*40, j*40, 40, 40)
+
+app = QtGui.QApplication(sys.argv)
+tg = TestGui()
+tg.show()
+app.exec_()
